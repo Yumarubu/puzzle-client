277222b24a5bad2ecc0228a47465ab27a815f359
Yumarubu <241208@st.yoshida-g.ac.jp>
2025-08-05 12:30:03 +0900

    ブロックをドラッグアンドドロップで移動できる要素を追加

---
+++ Assets/Scripts/BlockDragDrop.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockDragDrop : MonoBehaviour
+{
+    private Vector3 offset;
+    private Vector3 screenPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        this.screenPoint = Camera.main.WorldToScreenPoint(transform.position);
+        this.offset = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z));
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 currentScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z);
+        Vector3 currentPosition = Camera.main.ScreenToWorldPoint(currentScreenPoint) + this.offset;
+        transform.position = currentPosition;
+    }
+}
